ee9ddd88217ee482d701c716bc8ad873be4eeac2
Danail Obreschkow <danail@EMSs-MacBook-Pro.local>
2020-06-23 16:20:04 +0800

    Update

--- scripts/wallaby.sh
+++ scripts/wallaby.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=surfsuite
 #
-#SBATCH --ntasks=5
+#SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --mem-per-cpu=16000
 
@@ -10,7 +10,7 @@ module load gfortran/6.3.0 hdf5/1.10.2
 
 for seed in {1..5}
 do
-   srun -n 1 /home/dobreschkow/stingray/stingray -parameterset wallaby-micro-hyades_$seed -parameterfile /home/dobreschkow/stingray/parameters.txt -logfile /home/dobreschkow/log_wallaby_micro_$seed.txt &
-   srun -n 1 /home/dobreschkow/stingray/stingray -parameterset wallaby-medi-hyades_$seed -parameterfile /home/dobreschkow/stingray/parameters.txt -logfile /home/dobreschkow/log_wallaby_medi_$seed.txt &
+   srun -n 1 /home/dobreschkow/stingray/stingray -parameterset wallaby-micro-hyades_$seed -parameterfile /home/dobreschkow/stingray/parameters.txt -logfile /home/dobreschkow/log_wallaby_micro_$seed.txt
+   srun -n 1 /home/dobreschkow/stingray/stingray -parameterset wallaby-medi-hyades_$seed -parameterfile /home/dobreschkow/stingray/parameters.txt -logfile /home/dobreschkow/log_wallaby_medi_$seed.txt
 done
 wait
